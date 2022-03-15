6f0b7dad669cc1c2ed745f0cb356c23da51be4ab
Theawesometurtl <95330177+Theawesometurtl@users.noreply.github.com>
2022-03-14 20:54:05 -0700

    almost done creating a system to store all robot info

--- Flappy Bridd/Assets/Scripts/robotCreator.cs
+++ Flappy Bridd/Assets/Scripts/robotCreator.cs
@@ -9,68 +9,98 @@ public class robotCreator : MonoBehaviour
     [SerializeField] Vector2 spawn;
     public int maxSpeed = 5;
     public float jumpForce;
+    private int[] neuronsPerColumn = {4, 5, 1};
 
-    // has an extra row zero just to end the list
-    private List<int> amountOfNeurons = new List<int>(4, 5, 1, 0);
 
-    //creates a list of all the robot Brains which are made up of lists of columns of neurons made up of lists of all the neuron weights
-    public List<List<List<List<float>>>> listOfRobotBrains = new List<List<List<List<float>>>>();
 
-    private void fillList(List<int> amountOfNeurons, int robotNum, int amountOfColumns) {
-        for (int i = 0; i < robotNum; i++)
+    public class robotBrain
+    {
+        public List<column> columnList = new List<column>();
+    }
+
+    public class column 
+    {
+        public List<neuron> neuronList = new List<neuron>();
+    }
+        
+    public class neuron 
+    {
+        public float neuronWeight = 0f;
+        public List<float> connectionWeights = new List<float>();
+    }
+
+
+
+
+
+    public robotBrain fillBrain(int[] neuronsPerColumn) {
+        robotBrain robot = new robotBrain();
+        //columns
+        for (int columns = 0; columns < neuronsPerColumn.Length; columns++)
         {
-            //masterlist
-            listOfRobotBrains.Add(new List<List<List<float>>>());
-            //current list of neurons has 3 rows (excluding the 0)
-            for (int column = 0; column < 3; column++)
+            column c = new column();
+            robot.columnList.Add(c);
+            //neurons
+            for (int neurons = 0; neurons < neuronsPerColumn[columns]; neurons++)
             {
-                //column list
-                listOfRobotBrains[i].Add(new List<List<float>>());
-                //amount of neurons list has the amount of neurons per row
-                for (int neuron = 0; neuron < amountOfNeurons[column]; neuron++)
-                {
-                    //adds all neurons into the rows
-                    listOfRobotBrains[i][column].Add(new List<float>());
-
-                    //loops for every neuron in the next column
-                    for (int weight = 0; weight < amountOfNeurons[column + 1 ] +1/*extra one for neuron weight, rest for connection weight*/; weight++)
+                neuron n = new neuron();
+                //connections (but none for last row)
+                if (neuronsPerColumn.Length != columns) {
+                for (int connections = 0; connections < neuronsPerColumn[columns++]; connections++)
                     {
-                        //adds a weight to the current neuron
-                        listOfRobotBrains[i][column][neuron].Add(0f);
+                        n.connectionWeights.Add(0f);
+                    } 
+                }
+                
+                robot.columnList[columns].neuronList.Add(n);
+            }
+        }
+        return robot;
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+public List<robotBrain> fillList(int amount, int[] neuronsPerRow) {
+    List<robotBrain> listOfRobotBrains = new List<robotBrain>();
+            for (int i = 0; i < amount; i++)
+            {
+                //masterlist
+                listOfRobotBrains.Add(new robotBrain(neuronsPerRow));
+                
+                for (int column = 0; column < neuronsPerRow.Length; column++)
+                {
+                    //column list
+                    listOfRobotBrains[i].neuronList = neuronsPerRow;
+                    //amount of neurons list has the amount of neurons per row
+                    for (int neuron = 0; neuron < amountOfNeurons[column]; neuron++)
+                    {
+                        //adds all neurons into the rows
+                        listOfRobotBrains[i][column].Add(new List<float>());
+
+                        //loops for every neuron in the next column
+                        for (int weight = 0; weight < amountOfNeurons[column + 1 ] +1; weight++)
+                        {
+                            //adds a weight to the current neuron
+                            listOfRobotBrains[i][column][neuron].Add(0f);
+                        }
                     }
                 }
             }
         }
-    }
-
-
-    private void mutateList(List<List<List<List<float>>>> listOfRobotBrains) {
-        for (int i = 0; i < robotNum; i++)
-        {
-            //masterlist
-
-            //current list of neurons has 3 rows (excluding the 0)
-            for (int column = 0; column < 3; column++)
-            {
-                //column list
-
-                //amount of neurons list has the amount of neurons per row
-                for (int neuron = 0; neuron < amountOfNeurons[column]; neuron++)
-                {
-                    //neuron list
-
-
-                    //loops for every neuron in the next column
-                    for (int weight = 0; weight < amountOfNeurons[column + 1] + 1 /*extra one for neuron weight, rest for connection weight*/; weight++)
-                    {
-                        //mutates weight in current neuron
-                        listOfRobotBrains[i][column][neuron].Add(0f); //DO SOMETHING TO IT HERE!!!
-                    }
-                }
-            }
-        }
-    }
-
+*/
 
 /*
 //duplicating more robots
@@ -79,18 +109,15 @@ private void createBridd() {
         newbridd.position = spawn; 
         //Destroy(newpipe, screenTime);
     }
-
+*/
     void Start()
     {
-        //creating all robots
-        foreach (robot in listOfRobots) {
-            createBridd(robot)
-        }
-
-
         Time.timeScale = 1;
+        robotBrain robot = fillBrain(neuronsPerColumn);
+        Debug.Log(robot);
+        
     }
-*/
+
     // Update is called once per frame
     void Update()
     {
--- Flappy Bridd/Assets/Scripts/robotScript.cs
+++ Flappy Bridd/Assets/Scripts/robotScript.cs
@@ -4,18 +4,14 @@ using UnityEngine;
 
 public class robotScript : MonoBehaviour
 {
-    private int amountOfInputNodes = 5;
-    private double nodeValue;
-    private double robotBrainColumn1();
-    private double robotBrainColumn2();
-    private double robotBrainColumn3();
+
     [SerializeField] Rigidbody2D rb;
 
 
-
+/*
 
 //calculating a column of nodes with their respective weights
-private calculatingWeights() {
+private int[] calculatingWeights() {
     //does this to every node in the column
     foreach (int y in nodesList()) {
         //resetting weightnum
@@ -25,69 +21,65 @@ private calculatingWeights() {
         {
             weightNum ++;
             weight = robotBrain[weightNum];
-            connectionsList.append(nodesList[y] * weight);
+            connectionsList.Add(nodesList[y] * weight);
             
         }
         
 
     }
     //I should wipe the previous nodes list clean
-    nodesList.clear();
+    int[] nodesList = new int[];
 
     //this will give us the amount of nodes we will need
     private int amountOfNodes = weightNum;
 
     //this variable is to remember what section of the list we're on
     private int connectionNum = 0;
+    int nodeValue;
+    int connectionNum;
 
     //this will loop and calculate every node's value
     for (int i = 0; i < amountOfNodes; i++)
     {
-        nodeValue = 0
+        nodeValue = 0;
        
 
         //this loops for the amount of connections per node
-        for (int i = 0; i < length(connectionsList) / amountOfNodes; i++)
+        for (int i = 0; i < connectionsList.Length / amountOfNodes; i++)
         {
             //adding all the connections together for the node
-            nodeValue += connectionsList[connectionNum]
+            nodeValue += connectionsList[connectionNum];
 
             //getting next connection
             connectionNum++;
             
         }
         //adding the node to the nodesList. I SHOULD ALSO CALCULATE THE NODE'S WEIGHT HERE IF I DECIDDE TO DO THAT
-        nodesList.append(nodeValue);
+        nodesList.Add(nodeValue);
         
     }
 
     //FINISHED!!!
-    return(nodesList());
+    return(nodesList);
     
 }
-
-    void Start()
-    {
-        
-    }
-
+*/
 
     void Update()
     {
-        nodesList.append(rb.position.y);
-        nodesList.append(pipeRb.position.x);
-        nodesList.append(pipeRb.position.y);
-        nodesList.append(rb.velocity.y);
-        calculatingWeights();
-        calculatingWeights();
-        calculatingWeights();
+        /*
+        nodesList.Add(rb.position.y);
+        nodesList.Add(pipeRb.position.x);
+        nodesList.Add(pipeRb.position.y);
+        nodesList.Add(rb.velocity.y);
+
 
         //if the jump node is above 0, then it jumps
         if (nodesList[0] > 0) {
-            rb.velocity = new Vector2(rb.velocity.x, 1f)
+            rb.velocity = new Vector2(rb.velocity.x, 1f);
         }
 
-        
+        */
 
     }
 }
